7c8a1fa943f09b9e32af5ca46c8f0e289693539d
konstantin_mikulin <mikulin.konstantin@gmail.com>
2025-11-03 20:53:25 +0300

    Done

---
+++ test.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  var whatOne = stdin.readLineSync();
+  var how = stdin.readLineSync();
+  var whatTwo = stdin.readLineSync();
+  stdout.writeln(whatTwo);
+  stdout.writeln(how);
+  stdout.writeln(whatOne);
+}
