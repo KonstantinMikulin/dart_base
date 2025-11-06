2e280ae276b2ca1b3734da6fb58142382542813b
konstantin_mikulin <mikulin.konstantin@gmail.com>
2025-11-06 16:26:30 +0300

    Done

--- test.dart
+++ test.dart
@@ -1,12 +1,27 @@
 import 'dart:io';
 
 void main() {
-  final int n = int.parse(stdin.readLineSync()!);
+  final int kingX = int.parse(stdin.readLineSync()!);
+  final int kingY = int.parse(stdin.readLineSync()!);
+  final int destinationX = int.parse(stdin.readLineSync()!);
+  final int destinationY = int.parse(stdin.readLineSync()!);
 
-  if (n % 2 == 0) {
-    stdout.writeln('Да');
+  if (kingX + 1 == destinationX || kingY + 1 == destinationY) {
+    stdout.writeln('YES');
+    return;
   }
-  if (n % 2 != 0) {
-    stdout.writeln('Нет');
+  if (kingX - 1 == destinationX || kingY - 1 == destinationY) {
+    stdout.writeln('YES');
+    return;
+  }
+  if (kingY + 1 == destinationX || kingX - 1 == destinationY) {
+    stdout.writeln('YES');
+    return;
+  }
+  if (kingY - 1 == destinationX || kingX + 1 == destinationY) {
+    stdout.writeln('YES');
+    return;
+  } else {
+    stdout.writeln('NO');
   }
 }
